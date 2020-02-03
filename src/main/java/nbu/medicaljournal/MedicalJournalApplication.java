41e6fa7e702f774baba31f70f6ae8dcbe0bc5176
etonchev <emil.krassimirov@gmail.com>
2020-02-03 20:03:39 +0200

    Adds project skeleton produced by Spring Initializr

---
+++ src/main/java/nbu/medicaljournal/MedicalJournalApplication.java
@@ -0,0 +1,12 @@
+package nbu.medicaljournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalJournalApplication.class, args);
+	}
+}
---
+++ src/test/java/nbu/medicaljournal/MedicalJournalApplicationTests.java
@@ -0,0 +1,12 @@
+package nbu.medicaljournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
