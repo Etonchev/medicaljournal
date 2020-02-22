9f45985284b65bc8ffd70ffdbe100a4302b61f87
etonchev <emil.krassimirov@gmail.com>
2020-02-22 13:11:13 +0200

    ExaminationRepository now extends from JpaSpecificationExecutor as well

--- src/main/java/nbu/medicaljournal/repository/ExaminationRepository.java
+++ src/main/java/nbu/medicaljournal/repository/ExaminationRepository.java
@@ -2,6 +2,8 @@ package nbu.medicaljournal.repository;
 
 import nbu.medicaljournal.model.ExaminationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ExaminationRepository extends JpaRepository<ExaminationEntity, String> {
+public interface ExaminationRepository extends JpaRepository<ExaminationEntity, String>,
+        JpaSpecificationExecutor<ExaminationEntity> {
 }
