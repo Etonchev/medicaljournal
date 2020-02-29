a28624d4680d5279e2b166f83d1ceba15ab10111
etonchev <emil.krassimirov@gmail.com>
2020-02-29 13:47:52 +0200

    Adds patient angular structure

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { DoctorGetComponent } from './doctor-get/doctor-get.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DoctorEditComponent } from './doctor-edit/doctor-edit.component';
+import { PatientAddComponent } from './patient-add/patient-add.component';
+import { PatientEditComponent } from './patient-edit/patient-edit.component';
+import { PatientGetComponent } from './patient-get/patient-get.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { DoctorEditComponent } from './doctor-edit/doctor-edit.component';
     DoctorAddComponent,
     DoctorListComponent,
     DoctorGetComponent,
-    DoctorEditComponent
+    DoctorEditComponent,
+    PatientAddComponent,
+    PatientEditComponent,
+    PatientGetComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
---
+++ ui/src/app/patient-get/patient-get.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PatientGetComponent } from './patient-get.component';
+
+describe('PatientGetComponent', () => {
+  let component: PatientGetComponent;
+  let fixture: ComponentFixture<PatientGetComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PatientGetComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PatientGetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
